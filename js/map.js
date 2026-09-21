40a7e15264cc3331296d8de6c21b07c86f867a3a
mymac <mymac@mymacs-MacBook.local>
2026-09-21 18:57:27 +0700

    konten statistik map fixed cluster

--- js/map.js
+++ js/map.js
@@ -331,21 +331,21 @@ const AgrisightMap = (function () {
         <div class="legend-item" data-cluster="1">
           <div class="legend-item-left">
             <span class="legend-color-box" style="background-color:#1b3b2b;"></span>
-            <span class="legend-item-name">Klaster 1: Mandiri & Resilien</span>
+            <span class="legend-item-name">Klaster 1: Daerah Stabil Pangan</span>
           </div>
           <span class="legend-item-count">${c1Count}</span>
         </div>
         <div class="legend-item" data-cluster="2">
           <div class="legend-item-left">
             <span class="legend-color-box" style="background-color:#d97706;"></span>
-            <span class="legend-item-name">Klaster 2: Berkembang & Waspada</span>
+            <span class="legend-item-name">Klaster 2: Daerah Potensial Pangan</span>
           </div>
           <span class="legend-item-count">${c2Count}</span>
         </div>
         <div class="legend-item" data-cluster="3">
           <div class="legend-item-left">
             <span class="legend-color-box" style="background-color:#b93822;"></span>
-            <span class="legend-item-name">Klaster 3: Rentan & Prioritas 3T</span>
+            <span class="legend-item-name">Klaster 3: Daerah Rentan Pangan</span>
           </div>
           <span class="legend-item-count">${c3Count}</span>
         </div>
